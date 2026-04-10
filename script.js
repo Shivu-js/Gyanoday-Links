09ec9ca9bca9f205f862ac75a429aa580460b67b
Shivu-js <sushil912031@gmail.com>
2026-04-10 15:03:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('share-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    if (navigator.share) {
+        navigator.share({
+            title: 'Gyanoday Ke Guruji',
+            text: 'Gyanoday Ke Guruji | Trust of 20M+ Student on Youtube from UP & Other States Board',
+            url: window.location.href
+        }).then(() => {
+            console.log('Shared successfully!');
+        }).catch(console.error);
+    } else {
+        alert("Web Share API not supported in this browser.");
+    }
+});
